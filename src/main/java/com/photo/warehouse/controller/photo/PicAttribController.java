669d62b095e0742f2d090ae685d96531f5454f2b
chendy <chen@dingying.com>
2018-12-22 11:19:58 +0800

    init

--- src/main/java/com/photo/warehouse/controller/photo/PicAttribController.java
+++ src/main/java/com/photo/warehouse/controller/photo/PicAttribController.java
@@ -20,9 +20,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 import javax.imageio.ImageIO;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
@@ -66,54 +64,6 @@ public class PicAttribController extends PicAttribBaseController<PicAttribBiz,Pi
     @Value("${hddTpath}")
     private String hddTpath;
 
-    /**
-     * 设置图片置顶操作
-     * @param stick
-     * @param picStickTime
-     * @param picAttrib
-     * @return
-     * @throws ParseException
-     */
-    @Transactional
-    @SystemControllerLog(descrption = "图片管理")
-    @RequestMapping(value = "/updateStick/{stick}/{picStickTime}",method = RequestMethod.PUT)
-    public ObjectRestResponse updateStick(@PathVariable("stick") Integer stick, @PathVariable("picStickTime")Integer picStickTime, @RequestBody List<PicAttrib> picAttrib) throws ParseException {
-
-        ObjectRestResponse<PicAttrib> entityObjectRestResponse = new ObjectRestResponse<>();
-
-       //存储操作的Id
-        List<String> stringId = new ArrayList<>();
-        for(PicAttrib picId : picAttrib){
-            stringId.add(picId.getVcPid());
-        }
-
-        //判断操作
-        if(stick != null && stick == 1){
-            entityObjectRestResponse.setActionType("设置置顶");
-        }else if(stick != null && stick == 0){
-            entityObjectRestResponse.setActionType("取消置顶");
-        }else{
-            entityObjectRestResponse.setActionType("异常处理");
-        }
-
-        try{
-            //循环修改
-            for(PicAttrib pic : picAttrib){
-                pic.setPicStickTime(picStickTime);
-                pic.setPicStick(stick);
-                baseBiz.updateStick(pic);
-            }
-            entityObjectRestResponse.setResultCode(ReturnCode.RES_SUCCESS);
-            entityObjectRestResponse.setMessage("设置成功");
-        }catch (Exception e){
-            entityObjectRestResponse.setResultCode(ReturnCode.RES_FAILED);
-            entityObjectRestResponse.setMessage("设置失败");
-            entityObjectRestResponse.setException(ExceptionUtil.getStackTrace(e));
-        }
-        entityObjectRestResponse.setStringList(stringId);
-
-        return entityObjectRestResponse;
-    }
 
     /**
      * 图片明细(点击率暂时注释掉)
@@ -191,48 +141,6 @@ public class PicAttribController extends PicAttribBaseController<PicAttribBiz,Pi
     }
 
     /**
-     * 我要上传->上传图片
-     * 多文件以及带参数上传
-     * @param file
-     * @param StringBicAttrib
-     * @throws IOException
-     */
-    @Transactional
-    @RequestMapping(value = "/upLoadImg",method = RequestMethod.POST)
-    public ObjectRestResponse upLoadImg(@RequestParam("file") List<MultipartFile> file,@RequestParam("picAttrib") String StringBicAttrib) throws IOException {
-
-        ObjectRestResponse entityObjectRestResponse = new ObjectRestResponse<>();
-
-        //将字符串转json格式
-        JSONObject jsStr = JSONObject.fromObject(StringBicAttrib);
-        //Json转对象
-        PicAttrib picAttrib = (PicAttrib) JSONObject.toBean(jsStr,PicAttrib.class);
-
-        //获取时间戳，用来做组Id
-        String gid = String.valueOf(System.currentTimeMillis());
-        picAttrib.setVcGroupid(gid);
-
-        //循环上传图片
-        for(int i = 0;i<file.size();i++){
-            if(file.size() >1){
-                ;picAttrib.setcGroup("1");
-            }else{
-                picAttrib.setcGroup("0");
-            }
-            //第一张作为默认图
-            if(i == 0){
-                upload.uploadFile(file.get(i),picAttrib,1);
-            }else{
-                upload.uploadFile(file.get(i),picAttrib,0);
-            }
-        }
-        return entityObjectRestResponse;
-    }
-
-
-
-
-    /**
      * 图片查看
      * @param pid
      * @param type
@@ -243,10 +151,12 @@ public class PicAttribController extends PicAttribBaseController<PicAttribBiz,Pi
     @ApiOperation("图片查看")
     @GetMapping("/selectImg/{pid}/{type}")
     public void getCode(@PathVariable("pid") String pid,@PathVariable("type") String type,HttpServletRequest request,HttpServletResponse response) throws Exception{
+        //查询出相关信息
         PicAttrib picAttrib = baseBiz.selectByVcPid(pid);
         if(picAttrib == null){
             return;
         }
+
         File originalFile = null;
         File directory = new File("");// 参数为空
         //工程的根目录
@@ -275,78 +185,6 @@ public class PicAttribController extends PicAttribBaseController<PicAttribBiz,Pi
         finally {
             os.close();
             in.close();
-        }
-    }
-
-    // 修改状态
-    @RequestMapping(value = "/updateStatus",method = RequestMethod.PUT)
-    public boolean updatePicAttribStatusById(String vcPid){
-        PicAttrib picAttrib = baseBiz.selectByVcPid(vcPid);
-        if(picAttrib == null){
-            return false;
-        }
-        if(picAttrib.getcStatus().equals("1")){
-            picAttrib.setcStatus("2");
-        }else if(picAttrib.getcStatus().equals("2")){
-            picAttrib.setcStatus("1");
-        }
-        baseBiz.updatePicById(picAttrib);
-        return true;
-    }
-
-    /**
-     * 删除
-     * @param picAttribs
-     * @return
-     */
-    @RequestMapping(value = "/delete",method = RequestMethod.PUT)
-    @ResponseBody
-    public ObjectRestResponse<PicAttrib> remove(@RequestBody List<PicAttrib> picAttribs){
-        ObjectRestResponse<PicAttrib> entityObjectRestResponse = new ObjectRestResponse<>();
-        for(PicAttrib picAttrib : picAttribs){
-            baseBiz.deleteById(picAttrib);
-            //如果是组图的话
-            if(picAttrib.getcGroup().equals("1")){
-                //重新查询出最早上传的一张图片进行修改成默认图片
-                PicAttrib topPicAttrib =  baseBiz.selectTopPicAttrib(picAttrib);
-                if(topPicAttrib != null){
-                    topPicAttrib.setcMain("1");
-                    baseBiz.updateSelectiveById(topPicAttrib);
-                }
-                entityObjectRestResponse.data((PicAttrib)picAttrib);
-            }
-
-        }
-        return entityObjectRestResponse;
-    }
-
-    /**
-     * 删除整个组图
-     */
-    @RequestMapping(value = "/deleteList",method = RequestMethod.POST)
-    @Transactional
-    public void deleteList(@RequestBody List<PicAttrib> picAttribs) throws IOException {
-        File directory = new File("");// 参数为空
-        //工程的根目录
-        String courseFile = directory.getCanonicalPath();
-        for(PicAttrib picAttrib : picAttribs){
-            PicComment picComment = new PicComment();
-            picComment.setVcPid(picAttrib.getVcPid());
-            picCommentBiz.deletePicComment(picComment);
-            PicInfo picInfo = new PicInfo();
-            picInfo.setVcPid(picAttrib.getVcPid());
-            picInfoBiz.deletePicInfo(picInfo);
-            DownloadForm downloadForm = new DownloadForm();
-            downloadForm.setVcPid(picAttrib.getVcPid());
-            downloadFormBiz.deleteDownloadForm(downloadForm);
-            Integer i = baseBiz.deletePicAttribByGid(picAttrib);
-            if(i != 0){
-                File file = new File(courseFile + "\\" + hddUpath + "\\" + picAttrib.getVcUpload());
-                File file2 = new File(courseFile + "\\" + hddTpath + "\\" + picAttrib.getVcThumb());
-                file.delete();
-                file2.delete();
-            }
-
         }
     }
 
@@ -389,21 +227,282 @@ public class PicAttribController extends PicAttribBaseController<PicAttribBiz,Pi
         return entityObjectRestResponse;
     }
 
+    /**
+     * 查看组图下的所有图片
+     * @param vcGroupid
+     * @return
+     */
     @RequestMapping(value = "/getPicAttribByGid/{vcGroupid}",method = RequestMethod.GET)
     public List<PicAttrib> getPicAttribByGid(@PathVariable("vcGroupid") String vcGroupid){
        return baseBiz.getPicAttribByGid(vcGroupid);
     }
 
+    /**
+     * 不知道有没有操作到，待前端确认
+     * @param vcPid
+     * @return
+     */
+    // 修改状态
+    @RequestMapping(value = "/updateStatus",method = RequestMethod.PUT)
+    public boolean updatePicAttribStatusById(String vcPid){
+        PicAttrib picAttrib = baseBiz.selectByVcPid(vcPid);
+        if(picAttrib == null){
+            return false;
+        }
+        if(picAttrib.getcStatus().equals("1")){
+            picAttrib.setcStatus("2");
+        }else if(picAttrib.getcStatus().equals("2")){
+            picAttrib.setcStatus("1");
+        }
+        baseBiz.updatePicById(picAttrib);
+        return true;
+    }
+
+    /**
+     * 我要上传->上传图片
+     * 多文件以及带参数上传
+     * @param file
+     * @param StringBicAttrib
+     * @throws IOException
+     */
+    @SystemControllerLog(descrption = "图片管理->上传图片")
+    @Transactional
+    @RequestMapping(value = "/upLoadImg",method = RequestMethod.POST)
+    public ObjectRestResponse upLoadImg(@RequestParam("file") List<MultipartFile> file,@RequestParam("picAttrib") String StringBicAttrib) {
+
+        ObjectRestResponse entityObjectRestResponse = new ObjectRestResponse<>();
+
+        //将字符串转json格式
+        JSONObject jsStr = JSONObject.fromObject(StringBicAttrib);
+        //Json转对象
+        PicAttrib picAttrib = (PicAttrib) JSONObject.toBean(jsStr,PicAttrib.class);
+
+        //存Id
+        List<String> stringList = new ArrayList<>();
+
+
+        //获取时间戳，用来做组Id
+        String gid = String.valueOf(System.currentTimeMillis());
+        picAttrib.setVcGroupid(gid);
+
+        try{
+            //循环上传图片
+            for(int i = 0;i<file.size();i++){
+                if(file.size() >1){
+                    ;picAttrib.setcGroup("1");
+                }else{
+                    picAttrib.setcGroup("0");
+                }
+                //第一张作为默认图
+                if(i == 0){
+                    String pid =  upload.uploadFile(file.get(i),picAttrib,1);
+                    stringList.add(pid);
+                }else{
+                    String pid =  upload.uploadFile(file.get(i),picAttrib,0);
+                    stringList.add(pid);
+                }
+            }
+            entityObjectRestResponse.setResultCode(ReturnCode.RES_SUCCESS);
+            entityObjectRestResponse.setMessage("上传成功");
+        }catch (Exception e){
+            entityObjectRestResponse.setResultCode(ReturnCode.RES_FAILED);
+            entityObjectRestResponse.setMessage("上传失败");
+            entityObjectRestResponse.setException(ExceptionUtil.getStackTrace(e));
+        }
+        entityObjectRestResponse.setActionType("上传图片");
+        //存Id
+        entityObjectRestResponse.setStringList(stringList);
+        return entityObjectRestResponse;
+    }
+
+    /**
+     * 设置图片置顶操作
+     * @param stick
+     * @param picStickTime
+     * @param picAttrib
+     * @return
+     * @throws ParseException
+     */
+    @Transactional
+    @SystemControllerLog(descrption = "图片管理->置顶操作")
+    @RequestMapping(value = "/updateStick/{stick}/{picStickTime}",method = RequestMethod.PUT)
+    public ObjectRestResponse updateStick(@PathVariable("stick") Integer stick, @PathVariable("picStickTime")Integer picStickTime, @RequestBody List<PicAttrib> picAttrib) throws ParseException {
+
+        ObjectRestResponse<PicAttrib> entityObjectRestResponse = new ObjectRestResponse<>();
+
+        //存储操作的Id
+        List<String> stringId = new ArrayList<>();
+        for(PicAttrib picId : picAttrib){
+            stringId.add(picId.getVcPid());
+        }
+        entityObjectRestResponse.setStringList(stringId);
+
+        //判断操作
+        if(stick != null && stick == 1){
+            entityObjectRestResponse.setActionType("设置置顶");
+        }else if(stick != null && stick == 0){
+            entityObjectRestResponse.setActionType("取消置顶");
+        }else{
+            entityObjectRestResponse.setActionType("异常处理");
+        }
+
+        try{
+            //循环修改
+            for(PicAttrib pic : picAttrib){
+                pic.setPicStickTime(picStickTime);
+                pic.setPicStick(stick);
+                baseBiz.updateStick(pic);
+            }
+            entityObjectRestResponse.setResultCode(ReturnCode.RES_SUCCESS);
+            entityObjectRestResponse.setMessage("设置成功");
+        }catch (Exception e){
+            entityObjectRestResponse.setResultCode(ReturnCode.RES_FAILED);
+            entityObjectRestResponse.setMessage("设置失败");
+            entityObjectRestResponse.setException(ExceptionUtil.getStackTrace(e));
+        }
+
+
+        return entityObjectRestResponse;
+    }
+
+    /**
+     * 删除整个组图
+     * @param picAttribs
+     * @throws IOException
+     */
+    @SystemControllerLog(descrption = "图片管理")
+    @RequestMapping(value = "/deleteList",method = RequestMethod.POST)
+    @Transactional
+    public ObjectRestResponse deleteList(@RequestBody List<PicAttrib> picAttribs) throws IOException {
+
+        ObjectRestResponse entityObjectRestResponse = new ObjectRestResponse<>();
+
+        File directory = new File("");// 参数为空
+        //工程的根目录
+        String courseFile = directory.getCanonicalPath();
+
+        List<String> stringId = new ArrayList<>();
+        for(PicAttrib pid : picAttribs){
+            stringId.add(pid.getVcPid());
+        }
+        entityObjectRestResponse.setStringList(stringId);
+        entityObjectRestResponse.setActionType("删除组图");
+        try{
+            for(PicAttrib picAttrib : picAttribs){
+                PicComment picComment = new PicComment();
+                picComment.setVcPid(picAttrib.getVcPid());
+                picCommentBiz.deletePicComment(picComment);
+                PicInfo picInfo = new PicInfo();
+                picInfo.setVcPid(picAttrib.getVcPid());
+                picInfoBiz.deletePicInfo(picInfo);
+                DownloadForm downloadForm = new DownloadForm();
+                downloadForm.setVcPid(picAttrib.getVcPid());
+                downloadFormBiz.deleteDownloadForm(downloadForm);
+                Integer i = baseBiz.deletePicAttribByGid(picAttrib);
+                if(i != 0){
+                    File file = new File(courseFile + "\\" + hddUpath + "\\" + picAttrib.getVcUpload());
+                    File file2 = new File(courseFile + "\\" + hddTpath + "\\" + picAttrib.getVcThumb());
+                    file.delete();
+                    file2.delete();
+                }
+            }
+            entityObjectRestResponse.setResultCode(ReturnCode.RES_SUCCESS);
+            entityObjectRestResponse.setMessage("删除组图成功");
+        }catch (Exception e){
+            entityObjectRestResponse.setException(ExceptionUtil.getStackTrace(e));
+            entityObjectRestResponse.setResultCode(ReturnCode.RES_FAILED);
+            entityObjectRestResponse.setMessage("删除组图失败");
+        }
+        return entityObjectRestResponse;
+    }
+
+    /**
+     * 删除单个图片（审核操作里面的删除单个图片）
+     * @param picAttribs
+     * @return
+     */
+    @Transactional
+    @SystemControllerLog(descrption = "审核管理")
+    @RequestMapping(value = "/delete",method = RequestMethod.PUT)
+    @ResponseBody
+    public ObjectRestResponse<PicAttrib> remove(@RequestBody List<PicAttrib> picAttribs){
+
+        ObjectRestResponse<PicAttrib> entityObjectRestResponse = new ObjectRestResponse<>();
+
+        List<String> stringId = new ArrayList<>();
+        for(PicAttrib pId : picAttribs){
+            stringId.add(pId.getVcPid());
+        }
+        entityObjectRestResponse.setStringList(stringId);
+        entityObjectRestResponse.setActionType("删除图片");
+        try{
+            for(PicAttrib picAttrib : picAttribs){
+                baseBiz.deleteById(picAttrib);
+                //如果是组图的话
+                if(picAttrib.getcGroup().equals("1")){
+                    //重新查询出最早上传的一张图片进行修改成默认图片
+                    PicAttrib topPicAttrib =  baseBiz.selectTopPicAttrib(picAttrib);
+                    if(topPicAttrib != null){
+                        topPicAttrib.setcMain("1");
+                        baseBiz.updateSelectiveById(topPicAttrib);
+                    }
+                    entityObjectRestResponse.data((PicAttrib)picAttrib);
+                }
+            }
+            entityObjectRestResponse.setMessage("删除单个图片成功");
+            entityObjectRestResponse.setResultCode(ReturnCode.RES_SUCCESS);
+        }catch (Exception e){
+            entityObjectRestResponse.setMessage("删除单个图片失败");
+            entityObjectRestResponse.setResultCode(ReturnCode.RES_FAILED);
+            entityObjectRestResponse.setException(ExceptionUtil.getStackTrace(e));
+        }
+
+        return entityObjectRestResponse;
+    }
+
+    /**
+     * 批量审核操作（采用，拒绝）
+     * @param status
+     * @param reason
+     * @param picAttribList
+     */
     @RequestMapping(value = "/updateStatusList/{status}",method = RequestMethod.PUT)
     @Transactional
-    public void updatePicAttribStatusList(@PathVariable("status") String status,@Param("reason") String reason,@RequestBody List<PicAttrib> picAttribList){
-        System.out.println(picAttribList.size());
-        for(PicAttrib picAttrib : picAttribList){
-            picAttrib =  baseBiz.selectById(picAttrib);
-            picAttrib.setcStatus(status);
-            picAttrib.setVcReason(reason);
-            baseBiz.updateSelectiveById(picAttrib);
+    @SystemControllerLog(descrption = "审核管理->审核图片")
+    public ObjectRestResponse updatePicAttribStatusList(@PathVariable("status") String status,@Param("reason") String reason,@RequestBody List<PicAttrib> picAttribList){
+
+        ObjectRestResponse<PicAttrib> entityObjectRestResponse = new ObjectRestResponse<>();
+
+        //存Id
+        List<String> stringId = new ArrayList<>();
+        for(PicAttrib pId : picAttribList){
+            stringId.add(pId.getVcPid());
         }
+        entityObjectRestResponse.setStringList(stringId);
+
+        if(status.equals("1")){
+            entityObjectRestResponse.setActionType("已采用");
+        }else if(status.equals("0")){
+            entityObjectRestResponse.setActionType("已拒绝");
+        }
+
+        try{
+            //循环审批
+            for(PicAttrib picAttrib : picAttribList){
+                picAttrib =  baseBiz.selectById(picAttrib);
+                picAttrib.setcStatus(status);
+                picAttrib.setVcReason(reason);
+                baseBiz.updateSelectiveById(picAttrib);
+            }
+            entityObjectRestResponse.setResultCode(ReturnCode.RES_SUCCESS);
+            entityObjectRestResponse.setMessage("审核操作成功");
+        }catch (Exception e){
+            entityObjectRestResponse.setResultCode(ReturnCode.RES_FAILED);
+            entityObjectRestResponse.setException(ExceptionUtil.getStackTrace(e));
+            entityObjectRestResponse.setMessage("审核操作失败");
+        }
+
+        return entityObjectRestResponse;
     }
 
 }
